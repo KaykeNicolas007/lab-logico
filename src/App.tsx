5e69de6ad445551cb02657dcf387b18f81c849c4
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-09-13 12:43:51 -0300

    feat: define a tela inicial da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+
 function App() {
   return (
     <>
-      <h1 className="underline">LabLógico</h1>
+      <h1 className="pt-8">LabLógico</h1>
+      <p>Digite uma fórmula de Lógica Proposicional abaixo.</p>
+      <Label className="mt-8">Fórmula</Label>
+      <Input
+        id="formula"
+        type="text"
+        placeholder="(p -> q) & !q | !p"
+        className="mt-2"
+      />
     </>
   );
 }
